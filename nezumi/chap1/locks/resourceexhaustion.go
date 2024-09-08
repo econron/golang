f7d3b2476846b1847d7800a4827ec85e046f1d02
econron <aroneconbusi5544@gmail.com>
2024-09-08 22:20:18 +0900

    Go言語で学ぶ並行処理の勉強

---
+++ nezumi/chap1/locks/resourceexhaustion.go
@@ -0,0 +1,59 @@
+package locks
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func ResourceExhaustion() {
+	fmt.Println("this is resource exhaustion process.")
+
+	var wg sync.WaitGroup
+	var sharedlock sync.Mutex
+	const runtime = 1*time.Second
+
+	greedyWorker := func() {
+		defer wg.Done()
+
+		var count int
+		for begin := time.Now();time.Since(begin) <= runtime; {
+			sharedlock.Lock()
+			time.Sleep(3*time.Nanosecond)
+			sharedlock.Unlock()
+			count++
+		}
+
+		fmt.Printf("Greedy worker was able to execute %v work loops\n", count)
+	}
+
+	politeWorker := func() {
+		defer wg.Done()
+
+		var count int
+		for begin := time.Now();time.Since(begin) <= runtime; {
+
+			sharedlock.Lock()
+			time.Sleep(1*time.Nanosecond)
+			sharedlock.Unlock()
+
+			sharedlock.Lock()
+			time.Sleep(1*time.Nanosecond)
+			sharedlock.Unlock()
+
+			sharedlock.Lock()
+			time.Sleep(1*time.Nanosecond)
+			sharedlock.Unlock()
+
+			count++
+		}
+
+		fmt.Printf("Police worker was able to execute %v work loops.\n", count)
+	}
+
+	wg.Add(2)
+	go greedyWorker()
+	go politeWorker()
+
+	wg.Wait()
+}
---
+++ nezumi/chap1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"chap1/locks"
+)
+
+func main() {
+	locks.Livelock()
+	locks.ResourceExhaustion()
+}
